82d2b2572799a48f92d9fe9ad747082720ac5c7f
trustsuperdev <mw.topdev@gmail.com>
2022-07-04 13:12:46 -0700

    update home page

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -5,7 +5,9 @@ export const Home: React.FC = () => (
   <div className={styles.page}>
     <div className={styles.homeHeader}>
       <Link href="/">
+        <a>
         <img src="/img/logo.png" height="69px" alt="" />
+        </a>
       </Link>
       <img src="/img/title.png" height="60px" alt="" />
       <div className={styles.border}></div>
