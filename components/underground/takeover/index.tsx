899aabc12516d426ed6c1417c5b37f48a8f531ec
trustsuperdev <mw.topdev@gmail.com>
2022-07-04 12:42:39 -0700

    add takeover

---
+++ components/underground/takeover/index.tsx
@@ -0,0 +1,8 @@
+import styles from './Takeover.module.scss';
+
+export const Takeover: React.FC = () => (
+  <div className={styles.page}>
+    <h1>The takeover</h1>
+    <img src="/img/takeover/takeover.jpg" alt="" />
+  </div>
+);
